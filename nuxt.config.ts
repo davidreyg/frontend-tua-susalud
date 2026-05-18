b1ff5aea52000485ad9b72ad08bb3a317f7db430
David Rey <neisserrey@upeu.edu.pe>
2026-05-18 15:10:15 -0500

    feat: :sparkles: update ESLint and Prettier configurations, add Tailwind CSS support
    - Refactored ESLint configuration to use default export.
    - Enhanced Nuxt configuration with additional modules including color mode and Tailwind CSS.
    - Updated package.json to include new dependencies for Tailwind CSS and related plugins.
    - Added Prettier configuration for consistent code formatting.
    - Created VSCode settings and extensions recommendations for improved development experience.
    - Introduced Tailwind CSS styles and animations in a new CSS file.
    - Added utility function for Tailwind CSS class handling.
    - Configured UI component settings for better integration with Tailwind CSS.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,53 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
-})
+
+  modules: [
+    "@nuxt/eslint",
+    "@nuxtjs/color-mode",
+    "motion-v/nuxt",
+    "@vueuse/nuxt",
+    "@nuxt/icon",
+    "@nuxt/fonts",
+  ],
+
+  imports: {
+    imports: [
+      {
+        from: "tailwind-variants",
+        name: "tv",
+      },
+      {
+        from: "tailwind-variants",
+        name: "VariantProps",
+        type: true,
+      },
+    ],
+  },
+
+  colorMode: {
+    storageKey: "frontend-tua-susalud-color-mode",
+    classSuffix: "",
+  },
+
+  icon: {
+    clientBundle: {
+      scan: true,
+      sizeLimitKb: 0,
+    },
+
+    mode: "svg",
+    class: "shrink-0",
+    fetchTimeout: 2000,
+    serverBundle: "local",
+  },
+
+  css: ["~/assets/css/tailwind.css"],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
