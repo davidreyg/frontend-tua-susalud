dcce44bbfb607eb4e2bf1eb6481311b09e2cdf81
David R. <neisserrey@upeu.edu.pe>
2026-05-18 22:16:01 -0500

    feat: :sparkles: mejorar el diseño del layout ajustando la clase del contenedor principal para ocupar toda la pantalla

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="flex">
+  <div class="flex h-screen overflow-hidden">
     <AppSidebar />
     <main
-      class="flex min-h-screen flex-1 flex-col overflow-auto pt-16 transition-all duration-300 lg:pt-0"
+      class="flex flex-1 flex-col overflow-auto pt-16 transition-all duration-300 lg:pt-0"
     >
       <div class="w-full flex-1 px-4 py-8 sm:px-6 lg:px-8">
         <NuxtPage />
