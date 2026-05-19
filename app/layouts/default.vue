66d8dd972c99021f25ab644acaa7f698675103af
David R. <neisserrey@upeu.edu.pe>
2026-05-19 01:04:49 -0500

    feat: :sparkles: simplificar la estructura del componente main en default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="flex h-screen overflow-hidden">
     <AppSidebar />
-    <main
-      class="flex flex-1 flex-col overflow-auto pt-16 transition-all duration-300 lg:pt-0"
-    >
+    <main class="flex flex-1 flex-col overflow-auto pt-16 transition-all duration-300 lg:pt-0">
       <div class="w-full flex-1 px-4 py-8 sm:px-6 lg:px-8">
         <NuxtPage />
       </div>
